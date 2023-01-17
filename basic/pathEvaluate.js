12c4530055e02c338077bb8ef130e57172929536
chenxiangan <1598828268@qq.com>
2023-01-17 16:20:44 +0800

    update code

--- basic/pathEvaluate.js
+++ basic/pathEvaluate.js
@@ -1,5 +1,3 @@
-//path.insertBefore
-//path.insertAfter
 // 分别是在当前节点的前后插入节点，如下：
 const fs = require("fs");
 const parser = require("@babel/parser")
@@ -7,7 +5,7 @@ const traverse = require("@babel/traverse").default
 const t = require("@babel/types")
 const {ConditionalExpression} = require("@babel/traverse/lib/path/inference/inferers");
 const generator = require("@babel/generator").default
-const jscode = fs.readFileSync("./source/useTraverseExtDemo.js", {
+const jscode = fs.readFileSync("./source/pathEvaluate.js", {
     encoding: "utf-8"
 });
 
@@ -15,9 +13,12 @@ const jscode = fs.readFileSync("./source/useTraverseExtDemo.js", {
 let ast = parser.parse(jscode)
 
 visitor = {
-    ConditionalExpression(path){
-        path.insertBefore(t.valueToNode("before"))
-        path.insertAfter(t.valueToNode("after"))
+    //通过https://astexplorer.net/，得知目标代码类型为CallExpression。
+    "CallExpression"(path) {
+        //可以直接执行代码，第一参数true表示可以计算，否则返回false，第二个值就是目标值。
+        const {confident, value} = path.evaluate();
+
+        confident && path.replaceInline(t.valueToNode(value))
     }
 }
 traverse(ast, visitor)
