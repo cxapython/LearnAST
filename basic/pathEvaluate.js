27c70022c0f00a0cb289e03a05e973a14eea209b
chenxiangan <1598828268@qq.com>
2023-01-17 16:22:01 +0800

    update code

--- basic/pathEvaluate.js
+++ basic/pathEvaluate.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const parser = require("@babel/parser")
 const traverse = require("@babel/traverse").default
 const t = require("@babel/types")
-const {ConditionalExpression} = require("@babel/traverse/lib/path/inference/inferers");
 const generator = require("@babel/generator").default
 const jscode = fs.readFileSync("./source/pathEvaluate.js", {
     encoding: "utf-8"
@@ -14,7 +13,7 @@ let ast = parser.parse(jscode)
 
 visitor = {
     //通过https://astexplorer.net/，得知目标代码类型为CallExpression。
-    "CallExpression"(path) {
+    CallExpression(path) {
         //可以直接执行代码，第一参数true表示可以计算，否则返回false，第二个值就是目标值。
         const {confident, value} = path.evaluate();
 
